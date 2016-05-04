d72ccddf606e98e80cd74ec313c8d0971b86d646
Yuma Arakawa <yuarkw@gmail.com>
2016-05-04 22:44:10 +0900

    uart_tx_char: add tx register polling
    And delete unreferenced codes.

--- uart_tx_char/main.c
+++ uart_tx_char/main.c
@@ -1,21 +1,9 @@
 #define PHY_BASE_ADDR	(0x3f000000)
-#define GPIO_BASE	(0x00200000)
-#define GPFSEL1		((volatile unsigned int *)(PHY_BASE_ADDR + GPIO_BASE + 0x04))
-#define GPFSEL_ALT0	(0b100)
-#define GPFSEL_ALT5	(0b010)
-#define GPPUD		((volatile unsigned int *)(PHY_BASE_ADDR + GPIO_BASE + 0x94))
-#define GPPUDCLK0	((volatile unsigned int *)(PHY_BASE_ADDR + GPIO_BASE + 0x98))
-#define GPIO_TXD0	(14)
-#define GPIO_PAD_WAIT	150
-#define UART0_BASE	(0x00201000)
-#define UART0_DR	((volatile unsigned int *)(PHY_BASE_ADDR + UART0_BASE + 0x00))
-#define UART0_IBRD	((volatile unsigned int *)(PHY_BASE_ADDR + UART0_BASE + 0x24))
-#define UART0_FBRD	((volatile unsigned int *)(PHY_BASE_ADDR + UART0_BASE + 0x28))
-#define UART0_LCRH	((volatile unsigned int *)(PHY_BASE_ADDR + UART0_BASE + 0x2c))
-#define UART0_CR	((volatile unsigned int *)(PHY_BASE_ADDR + UART0_BASE + 0x30))
 #define AUX_BASE	(0x00215000)
 #define AUX_ENABLES	((volatile unsigned int *)(PHY_BASE_ADDR + AUX_BASE + 0x04))
 #define MU_BASE		(PHY_BASE_ADDR + 0x00215040)
+#define MU_LSR_TX_IDLE	(1U << 6)
+#define MU_LSR_TX_EMPTY	(1U << 5)
 
 struct bcm283x_mu_regs {
 	unsigned int io;
@@ -31,19 +19,13 @@ struct bcm283x_mu_regs {
 	unsigned int baud;
 };
 
-/* volatile unsigned int this_cpuid = (unsigned int)'0'; */
-
 int main(void)
 {
-	volatile unsigned int i;
-	struct bcm283x_mu_regs *mu = (struct bcm283x_mu_regs *)MU_BASE;
+	volatile struct bcm283x_mu_regs *mu = (struct bcm283x_mu_regs *)MU_BASE;
 	char ch = 'A';
 
-	/* this_cpuid++; */
-
 	while (1) {
-		for (i = 0; i < 5000000; i++);
-		/* mu->io = this_cpuid; */
+		while (!(mu->lsr && MU_LSR_TX_IDLE) && !(mu->lsr && MU_LSR_TX_EMPTY));
 		mu->io = ch++;
 		if (ch > 'Z')
 			ch = 'A';
